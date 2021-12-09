758ef8293c4e9c32165e05e48f87c68355fdcbc6
mehdi-elke <m.elketroussi@gmail.com>
2021-12-09 22:04:33 +0100

    remove useless method

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -26,17 +26,6 @@ final class FizzBuzz{
         return strval($value);
     }
 
-    public static function execute3( $value): String {
-        
-        if($value%15==0){
-            return "FizzBuzz";
-        } elseif( $value%3 == 0){
-            return "Fizz";
-        } elseif ($value%5==0){
-            return "Buzz";
-        }
-        return strval($value);
-    }
 
     public static function noOp( $value): void {
         print($value);
