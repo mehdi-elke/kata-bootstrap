0b782dda844d2ce9e240c3b0ce9bc9700b9abcd6
mehdi-elke <m.elketroussi@gmail.com>
2022-01-06 16:18:43 +0100

    add code

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -10,6 +10,38 @@ final class FizzBuzz{
         } elseif ($value%5 == 0){
             return "buzz";
         }
+        if($value%15 == 0){
+            return "fizzbuzz";
+        }elseif( $value%3 == 0){
+            return "fizz";
+        } elseif ($value%5 == 0){
+            return "buzz";
+        }
+        if($value%15 == 0){
+            return "fizzbuzz";
+        }elseif( $value%3 == 0){
+            return "fizz";
+        } elseif ($value%5 == 0){
+            return "buzz";
+        }
+        if($value%15 == 0){
+            return "fizzbuzz";
+        }elseif( $value%3 == 0){
+            return "fizz";
+        } elseif ($value%5 == 0){
+            return "buzz";
+        }
+        return strval($value);
+    }
+
+    public static function execute2($value): String{
+        if($value%15 == 0){
+            return "fizzbuzz";
+        }elseif( $value%3 == 0){
+            return "fizz";
+        } elseif ($value%5 == 0){
+            return "buzz";
+        }
         return strval($value);
     }
 
